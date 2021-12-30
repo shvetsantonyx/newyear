5cac43c39f5e198763b9438de517c0fa78381dc5
unknown <invader@mail.ru>
2021-12-30 21:27:50 +0300

    Debug=False

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0oa8jh@%der23@sbcu2p4*m!!7%k-y51ul5%s0-xniy(bc5&g3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
